8f5a406c9f8d40b4e6253ade9faafb34444541d1
Mitchell Rose <rosem1696@gmail.com>
2021-09-28 23:49:20 -0500

    adding frontend dist to gitignore

--- packages/frontend/dist/src/app/app.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import { appTheme } from './app.scss';
-export class App extends React.Component {
-    render() {
-        return _jsx("h1", Object.assign({ className: appTheme }, { children: "Web Template" }), void 0);
-    }
-}
-//# sourceMappingURL=app.js.map
